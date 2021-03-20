6e28ded4ce9af8d537bd1caf7a15caa52539f087
chase-klaus <chase.klaus@gmail.com>
2021-03-19 20:14:29 -0400

    updated preliminary user schema

--- router.js
+++ router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const userController = require('./controllers/user.controller');
 
 router.post('/register', userController.createUser);
+router.get('/login', userController.login);
 
 module.exports = router;
