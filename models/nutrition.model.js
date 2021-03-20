406a8b8f0eb84293b332afde1bd5b884c80a54ac
Deida <deidalopez1@gmail.com>
2021-03-20 11:09:22 -0700

    input info into baseline.json

---
+++ models/nutrition.model.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require('.');
+
+const NutrientsSchema = new Schema({
+  protein: {
+    type: Number
+  },
+  fluids: {
+    type: Number
+  },
+  fiber: {
+    type: Number
+  },
+  vitaminA: {
+    type: Number
+  },
+  thiamin: {
+    type: Number
+  },
+  riboflavin: {
+    type: Number
+  },
+  niacin: {
+    type: Number
+  },
+  vitaminB6: {
+    type: Number
+  },
+  vitaminB12: {
+    type: Number
+  },
+  folate: {
+    type: Number
+  },
+  vitaminC: {
+    type: Number
+  },
+  calcium: {
+    type: Number
+  },
+  iron: {
+    type: Number
+  },
+  magnesium: {
+    type: Number
+  },
+  potassium: {
+    type: Number
+  },
+  sodium: {
+    type: Number
+  },
+  zinc: {
+    type: Number
+  }
+});
+
+
+module.exports = model('User', NutrientsSchema);
+
