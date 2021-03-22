d338e91e594f2d7245d4d8951717a591b00c6f61
chase-klaus <chase.klaus@gmail.com>
2021-03-22 09:12:01 -0400

    updated models, added more user controllers, commmented out JWT for now

---
+++ models/item.model.js
@@ -0,0 +1,72 @@
+const { Schema, model } = require('.');
+
+const NutrientsSchema = new Schema({
+    protein: {
+        type: Number
+    },
+    fluids: {
+        type: Number
+    },
+    fiber: {
+        type: Number
+    },
+    vitaminA: {
+        type: Number
+    },
+    thiamin: {
+        type: Number
+    },
+    riboflavin: {
+        type: Number
+    },
+    niacin: {
+        type: Number
+    },
+    vitaminB6: {
+        type: Number
+    },
+    vitaminB12: {
+        type: Number
+    },
+    folate: {
+        type: Number
+    },
+    vitaminC: {
+        type: Number
+    },
+    calcium: {
+        type: Number
+    },
+    iron: {
+        type: Number
+    },
+    magnesium: {
+        type: Number
+    },
+    potassium: {
+        type: Number
+    },
+    sodium: {
+        type: Number
+    },
+});
+
+const ItemSchema = new Schema({
+    user: {
+        type: String || Number // not quite sure yet
+    },
+    dateCreated: {
+        type: Date
+    },
+    itemName: {
+        type: String,
+    },
+    servingQuantity: {
+        type: Number,
+    },
+    nutrients: {
+        nutrients: [NutrientsSchema],
+    }
+});
+
+module.exports = model('Item', ItemSchema);
--- models/user.model.js
+++ models/user.model.js
@@ -1,77 +1,5 @@
 const { Schema, model } = require('.');
 
-const NutrientsSchema = new Schema({
-    protein: {
-        type: Number
-    },
-    fluids: {
-        type: Number
-    },
-    fiber: {
-        type: Number
-    },
-    vitaminA: {
-        type: Number
-    },
-    thiamin: {
-        type: Number
-    },
-    riboflavin: {
-        type: Number
-    },
-    niacin: {
-        type: Number
-    },
-    vitaminB6: {
-        type: Number
-    },
-    vitaminB12: {
-        type: Number
-    },
-    folate: {
-        type: Number
-    },
-    vitaminC: {
-        type: Number
-    },
-    calcium: {
-        type: Number
-    },
-    iron: {
-        type: Number
-    },
-    magnesium: {
-        type: Number
-    },
-    potassium: {
-        type: Number
-    },
-    sodium: {
-        type: Number
-    },
-});
-
-const ItemsConsumedSchema = new Schema({
-    itemName: {
-        type: String,
-    },
-    servingQuantity: {
-        type: Number,
-    },
-    nutrients: {
-        nutrients: [NutrientsSchema],
-    }
-});
-
-const SavedDaysSchema = new Schema({
-    date: {
-        type: Date,
-    },
-    itemConsumed: {
-        items: [ItemsConsumedSchema],
-    }
-});
-
 const UserSchema = new Schema({
     email: {
         type: String,
@@ -98,16 +26,8 @@ const UserSchema = new Schema({
     },
     dietChoice: {
         type: String,
-    },
-    achievements: {
-        type: Array,
-    },
-    days: {
-        savedDays: [SavedDaysSchema],
     }
 });
-
-
 
 module.exports = model('User', UserSchema);
 
