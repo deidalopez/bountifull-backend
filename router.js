7b1dadf7cb435e9e05cdfd957c11a6e729685c3f
chase-klaus <chase.klaus@gmail.com>
2021-03-22 14:10:53 -0400

    updateUser, getUserById fixed

--- router.js
+++ router.js
@@ -5,11 +5,12 @@ const itemsController = require('./controllers/items.controllers');
 
 router.post('/register', userController.createUser);
 router.post('/login', userController.login);
+router.get('/user/:id', userController.getUserById);
 router.get('/profile', authMiddleware, userController.profile);
-router.get('/getUser', userController.getUserByEmail);
+router.get('/getUserByEmail', userController.getUserByEmail);
 router.get('/getAllUsers', userController.getAllUsers);
 router.delete('/deleteUser', userController.deleteUser);
-router.put('/update', userController.updateUser);
+router.put('/update/:id', userController.updateUser);
 
 
 router.post('/addItem', itemsController.addItem);
