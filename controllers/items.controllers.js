f597b7c8ce88156e040bb4e17a6d71752ca1c034
Deida <deidalopez1@gmail.com>
2021-03-30 13:43:54 -0700

    premerge

--- controllers/items.controllers.js
+++ controllers/items.controllers.js
@@ -6,19 +6,19 @@ const APIUrl = process.env.API_URL;
 const APIKey = process.env.API_KEY;
 
 const addItem = async (req, res) => {
-  const { itemName, user, servingQuantity, totalNutrients, uniqueId, totalGoalMet  } = req.body;
+  const { itemName, user, servingQuantity, totalNutrients, uniqueId, totalGoalMet } = req.body;
+  // will fix
   const dateToday = new Date().toISOString().substring(0, 10);
   try {
-    console.log(itemName, user, servingQuantity, totalNutrients);
     const newItem = await Item.create({
-      uniqueId: uniqueId,
-      itemName: itemName,
-      user: user,
-      servingQuantity: servingQuantity,
+      uniqueId,
+      itemName,
+      user,
+      servingQuantity,
       dateCreated: dateToday,
-      totalNutrients: totalNutrients
+      totalNutrients
     });
-    const currentUser = await User.findOne({ _id: user});
+    const currentUser = await User.findOne({ _id: user });
     const updatedGoalEntry = {
       date: dateToday,
       totalGoalMet: totalGoalMet
@@ -37,7 +37,6 @@ const addItem = async (req, res) => {
     }
 
     res.status(200).send(newItem);
-    console.log(newItem);
   } catch (error) {
     res.status(500).json({ message: error });
   }
@@ -45,8 +44,7 @@ const addItem = async (req, res) => {
 
 const getItemsByUserAndDate = async (req, res) => {
   const user = req.params.id;
-  const dateCreated =  req.params.date;
-  console.log(user);
+  const dateCreated = req.params.date;
   try {
     const foundItems = await Item.find({ user: user, dateCreated: dateCreated }).exec();
     res.status(200).send(foundItems);
@@ -56,9 +54,7 @@ const getItemsByUserAndDate = async (req, res) => {
 };
 
 const deleteItemById = async (req, res) => {
-  console.log('delete ')
-    const uniqueId = req.params.id 
-  console.log(uniqueId)
+  const uniqueId = req.params.id
   try {
     const deletedItem = await Item.findOneAndDelete({ uniqueId: uniqueId });
     res.status(200).send(deletedItem);
@@ -79,21 +75,21 @@ const updateById = async (req, res) => {
 
 const search = async (req, res) => {
   const { type, query } = req.body;
-  if (!type || !query) return res.status(400).json({error: 'Bad request: Please provide a valid request type and query!'});
+  if (!type || !query) return res.status(400).json({ error: 'Bad request: Please provide a valid request type and query!' });
   try {
-    const { data } = await axios.get(`${APIUrl}/search?api_key=${APIKey}&query=${query}${ type !== 'all' ?
+    const { data } = await axios.get(`${APIUrl}/search?api_key=${APIKey}&query=${query}${type !== 'all' ?
       type === 'non' ?
         '&dataType=Foundation,Survey,SR%20Legacy' : '&dataType=Branded'
-      : '' }`);
+      : ''}`);
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json({error: err});
+    res.status(500).json({ error: err });
   }
 };
 
 const getNutrition = async (req, res) => {
   const { id } = req.params;
-  if (!id) return res.status(400).json({error: 'Bad request: Please provide id!'});
+  if (!id) return res.status(400).json({ error: 'Bad request: Please provide id!' });
   try {
     const { data } = await axios.post(`${APIUrl}?api_key=${APIKey}`, {
       fdcIds: [id],
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,15 +2,14 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Item = require('../models/item.model');
 const User = require('./../models/user.model');
-// const { getItemsByUserAndDate } = require('./items.controllers');
 const SECRET_KEY = process.env.SECRET_KEY;
 
 const createUser = async (req, res) => {
-    // if ((req.body.email) && (req.body.email).length < 1) res.status(400).send({ error, message: "Please enter email" });
-    // if ((req.body.password).length < 6) res.status(400).send({ error, message: "Password must be at least 6 characters" });
-    const user = await User.findOne({ email: req.body.email });
-    if (user) res.status(409).send({ error: '409', message: 'User already exists ' });
+    if ((req.body.email) && (req.body.email).length < 1) res.status(400).send({ error, message: "Please enter email" });
+    if ((req.body.password).length < 6) res.status(400).send({ error, message: "Password must be at least 6 characters" });
     try {
+        const user = await User.findOne({ email: req.body.email });
+        if (user) res.status(409).send({ error: '409', message: 'User already exists ' });
         if (req.body.password === '') throw new Error();
         const hash = await bcrypt.hash(req.body.password, 10);
         const newUser = new User({ ...req.body, password: hash });
@@ -23,12 +22,12 @@ const createUser = async (req, res) => {
 };
 
 const login = async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) res.status(400).send({ message: 'User not found' });
-    const today = new Date().toISOString().substring(0, 10);
-    const allItems = await Item.find({ user: user._id, dateCreated: today }).exec();
-    if (!allItems) allItems = [];
     try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) res.status(400).send({ message: 'User not found' });
+        const today = new Date().toISOString().substring(0, 10);
+        const allItems = await Item.find({ user: user._id, dateCreated: today }).exec();
+        if (!allItems) allItems = [];
         const validatePass = await bcrypt.compare(req.body.password, user.password);
         if (!validatePass) res.status(400).send({ error, message: 'Incorrect username and/or password' });
         const token = jwt.sign({ _id: user._id }, SECRET_KEY);
@@ -48,19 +47,8 @@ const profile = async (req, res) => {
     }
 };
 
-const getAllUsers = async (req, res) => {
-    const users = await User.find();
-    try {
-        res.json(users);
-        res.status(200);
-    } catch (error) {
-        res.status(500).send({ error, message: 'Could not get all users' });
-    }
-};
-
 const deleteUser = async (req, res) => {
     const { _id } = req.body;
-    console.log(_id);
     try {
         const deletedUser = await User.deleteOne({ _id: _id });
         res.status(200).send(deletedUser);
@@ -81,21 +69,23 @@ const updateUser = async (req, res) => {
 const getUserById = async (req, res) => {
     try {
         const user = await User.findById({ _id: req.params.id });
-        console.log(user);
         res.status(200).send(user);
     } catch (error) {
         res.status(500).send('Could not find by id');
     }
 };
 
-const getUserByEmail = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.find({ email: email, password: password });
-        res.status(200).send(user);
-    } catch (error) {
-        res.status(500).send({ error, message: 'Could not find user by email' });
-    }
-};
+// const getTotalGoalMet = async (req, res) => {
+//     // day _id
+//     // const {_id} = req.body
+//     const {_id} = req.body
+    
+//     try {
+//         const userInfo = User.findById({_id: _id})
+//         console.log(userInfo);
 
-module.exports = { createUser, login, profile, getAllUsers, deleteUser, getUserByEmail, updateUser, getUserById };
+//         const dayInfo = User.findById({_id: _id})
+//     }
+// }
+
+module.exports = { createUser, login, profile, deleteUser, updateUser, getUserById };
