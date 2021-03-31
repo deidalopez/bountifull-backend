a95c3a6e71f83bb44fdf41c01106fc6cc59eb55f
Deida <deidalopez1@gmail.com>
2021-03-31 06:39:24 -0700

    Testing server for deployment

--- index.js
+++ index.js
@@ -15,4 +15,9 @@ app.use(cors(corsConfig));
 app.use(express.json());
 app.use(router);
 
+
+app.get('/', (req, res) => {
+    res.send('<h1>WELCOME TO THE SERVER</h1>')
+})
+
 app.listen(SERVER_PORT, () => console.log(`Server is running on http://localhost:${SERVER_PORT}`));
