b4af9ed61e47cdd0e0c2932339b4c336ea9ed8d4
Deida <deidalopez1@gmail.com>
2021-03-31 07:47:52 -0700

    cleaning up index

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const ATLAS_USERNAME = process.env.ATLAS_USERNAME;
 const ATLAS_PASSWORD = process.env.ATLAS_PASSWORD;
 
-mongoose.connect(`mongodb+srv://${ATLAS_USERNAME}:${ATLAS_PASSWORD}@bountifull.21g8e.mongodb.net/bountifull?retryWrites=true&w=majority?authSource=admin`,
+mongoose.connect(`mongodb+srv://${ATLAS_USERNAME}:${ATLAS_PASSWORD}@bountifull.21g8e.mongodb.net/bountifull?retryWrites=true&w=majority`,
     { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false },
     (err) => {
         if (err) {
